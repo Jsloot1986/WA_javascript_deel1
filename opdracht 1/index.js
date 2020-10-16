c78a95d904811ee73668b4b63d8b31ef6db31316
Jochum Sloot <67738459+Jsloot1986@users.noreply.github.com>
2020-10-16 13:11:28 +0200

    Javascript opdracht 1
    Javascript basics opdracht 1

---
+++ opdracht 1/index.js
@@ -0,0 +1,21 @@
+let name = 'Jochum';
+//if kan niet als naam, omdat het een keyword is!
+let result = 4+4;
+//uitkomst 8, kan wel!
+let resultTwo = '4'+'4';
+//uitkomst 44, omdat hij de twee strings combineert, kan dus niet!
+let resultPlus = 3+3;
+//uitkomst 6
+let resultMin = 3-3;
+//uitkomst 0
+let resultKeer = 3*3;
+//uitkomst 9
+let resultDelen = 3/3;
+//uitkomst 1
+let resultProc = 3%3;
+//uitkomst 0
+let age = 34;
+// number
+let ageOne = '34';
+// string
+console.log(typeof ageOne);
