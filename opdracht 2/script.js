eee0aec875e444ff82da50547555ca235624b526
Jochum Sloot <67738459+Jsloot1986@users.noreply.github.com>
2020-10-17 13:43:27 +0200

    Javascript in Command prompt checken
    Javascript code in commend promp line checken! oefening

---
+++ opdracht 2/script.js
@@ -0,0 +1,8 @@
+/*let result = 1230941*1823794;
+Dit is de uitkomst van een grote som (je ziet de comments niet in je command prompt)
+console.log(result);//2244982810154
+let resultDelen = 637263 / 54;
+console.log (resultDelen);//11801.166666666666*/
+//na dit allemaal uitgecommand te hebben zie je niks alleen de onderste regel blijft zichtbaar.
+
+console.log("Hallo Winc Academy studenten")
